f0ce95db5ee9d2faea542fa3311bc33c5c7b1b19
laqudee <2268678583@qq.com>
2023-05-12 21:26:13 +0800

    feat 基本算法rust构建栈

--- chapter-3/src/main.rs
+++ chapter-3/src/main.rs
@@ -1,3 +1,14 @@
+mod stack;
+
+use stack::Stack;
+
 fn main() {
-    println!("Hello, world!");
+    // Test Stack
+    let mut s = Stack::new();
+    s.push(1);
+    s.push(2);
+    s.push(4);
+    println!("top {:?}, size {:?}", s.peek().unwrap(), s.size());
+    println!("pop {:?}, size {:?}", s.pop().unwrap(), s.size());
+    println!("is_empty: {}, stack:{:?}", s.is_empty(), s);
 }
